91562545d56ce13497a5d0e0edef3902344bfa80
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-05-13 18:14:07 +0300

    Insert Sorting method to our bookings

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -78,7 +78,7 @@ function CabinRow({ cabin }) {
       <Table.Row>
         <Img src={image} />
         <Cabin>{name}</Cabin>
-        <div>{maxCapacity}</div>
+        <div>Fits up to {maxCapacity} guests</div>
         <Price>{formatCurrency(regularPrice)}</Price>
         {discount ? (
           <Discount>{formatCurrency(discount)}</Discount>
